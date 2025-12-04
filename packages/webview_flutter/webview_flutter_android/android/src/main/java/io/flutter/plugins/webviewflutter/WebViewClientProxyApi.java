65e984934dd44fc50675d8a338db11ec83d099f1
Taewoong Park <taewoong.park@jobis.co>
2025-12-04 12:22:53 +0900

    Revert "feat: ssl 이슈 수정"
    This reverts commit 84c0f3ec16e67af7d84425b1a9289ae5c892a3e7.

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/WebViewClientProxyApi.java
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/WebViewClientProxyApi.java
@@ -143,9 +143,8 @@ public class WebViewClientProxyApi extends PigeonApiWebViewClient {
         @NonNull android.webkit.WebView view,
         @NonNull android.webkit.SslErrorHandler handler,
         @NonNull android.net.http.SslError error) {
-      handler.proceed();
-//      api.getPigeonRegistrar()
-//          .runOnMainThread(() -> api.onReceivedSslError(this, view, handler, error, reply -> null));
+      api.getPigeonRegistrar()
+          .runOnMainThread(() -> api.onReceivedSslError(this, view, handler, error, reply -> null));
     }
 
     @Override
